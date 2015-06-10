8d0269bb1ed2fac216308bf26a7d05cf67fc438b
hpetrow <heather.petrow@gmail.com>
2015-06-10 19:31:52 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # Write code here
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each_with_object({}) { |(grade, students), hash|
+      hash[grade] = students.sort
+    }
+  end
+end
